a64beb055cdf950697dfbf11ee0b849f38bdb7bb
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2024-01-20 10:53:05 +0530

    feat: implement socket io

--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import {postApiSignup, postApiLogin} from "./controllers/user.js";
+import { postApiSignup, postApiLogin } from "./controllers/user.js";
+import { Server } from "socket.io";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+const io = new Server(5005, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("message", (data) => {
+    console.log(data);
+  });
+});
+
+app.get("/sendMessage", (req, res) => {
+  const { message } = req.query;
+  io.emit("receive", message);
+
+  res.status(200).json({ message: "Message send" });
+});
 
 async function connectMongoDB() {
   try {
--- server/models/Message.js
+++ server/models/Message.js
@@ -1,19 +1,22 @@
 import { Schema, model } from "mongoose";
 
-const messageSchema = new Schema ({
+const messageSchema = new Schema(
+  {
     message: String,
     sender: {
-        type:Schema.Types.ObjectId,
-        ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
     receiver: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-},{
-    timestamps: true
-})
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Message = model('Message', messageSchema);
+const Message = model("Message", messageSchema);
 
 export default Message;
